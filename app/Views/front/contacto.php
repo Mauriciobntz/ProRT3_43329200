5f2bfee7e0a2a2a915726c72dd6b4e206b05edbe
Mauriciobntz <mauriciobenitezok@gmail.com>
2024-08-15 00:04:51 -0300

    sexto commit

---
+++ app/Views/front/contacto.php
@@ -0,0 +1,22 @@
+<div class="container mt-5">
+    <h2 class="mb-4">Formulario de Contacto</h2>
+    <form>
+        <div class="form-group">
+            <label for="name">Nombre:</label>
+            <input type="text" class="form-control" id="name" placeholder="Tu nombre" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Correo Electrónico:</label>
+            <input type="email" class="form-control" id="email" placeholder="Tu correo electrónico" required>
+        </div>
+        <div class="form-group">
+            <label for="subject">Asunto:</label>
+            <input type="text" class="form-control" id="subject" placeholder="Asunto" required>
+        </div>
+        <div class="form-group">
+            <label for="message">Mensaje:</label>
+            <textarea class="form-control" id="message" rows="4" placeholder="Tu mensaje" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+</div>
